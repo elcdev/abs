fe7c5102ed7eb123174c651ac01b432e9ac73375
media <dexter.developer@gmail.com>
2018-06-28 01:47:04 +0300

    Menu table update

---
+++ db/dump/menu.d
@@ -0,0 +1,13 @@
+1 1 0 2018-06-28T01:19:08.622 "admin" 2018-06-28T01:19:08.622 "admin" 1 "CLIENTS" "clientsDb" "/pages/clientsdb/" "Client database"
+.
+PSC
+filename=menu
+records=0000000000001
+ldbname=absdb
+timestamp=2018/06/28-01:24:51
+numformat=44,46
+dateformat=mdy-1950
+map=NO-MAP
+cpstream=ISO8859-1
+.
+0000000134
