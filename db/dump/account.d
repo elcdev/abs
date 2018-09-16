988be61c5f7a425235bfa4b7022396a204dae070
cido <cidocido@gmail.com>
2018-09-16 21:39:19 +0300

    Test accounts update

--- db/dump/account.d
+++ db/dump/account.d
@@ -1,14 +1,15 @@
 1 0 0 2018-09-08T03:12:16.927 "user" 2018-09-08T03:12:16.927 "user" 10212000 "" "CBLT.EUR" "CBLT.EUR" "" "EUR" "" "DFB" "SCA" "" "" "0" 0 "ACTIVE" "CENTRAL BANK OF LITHUANIA (Clients Funds)" 07/25/18 ? 0 07/25/18 07/25/18 500 ? 0
-2 0 0 2018-09-08T03:18:37.170 "user" 2018-09-08T03:18:37.170 "user" 30311000 "" "123456789012" "123456789012" "" "EUR" "123" "CIF" "" "" "1" "0" 0 "Active" "Test client Account" 07/25/18 07/25/18 0 07/25/18 ? 0 07/25/18 500
+2 837 0 2018-09-16T21:27:26.489 "media" 2018-09-08T03:18:37.170 "user" 30311000 "" "123456789012" "123456789012" "" "EUR" "123" "CIF" "" "" "1" "0" 0 "Active" "Test client Account" 07/25/18 07/25/18 0 09/12/18 ? 0 07/25/18 500
+5 833 0 2018-09-16T21:27:22.018 "media" 2018-09-16T19:37:37.265 "media" 30311000 "" "C00000000003" "C00000000003" "" "EUR" "123" "CIF" "" "" "1" "0" 0 "Active" "Test account for client 2" 07/25/18 07/25/18 0 09/12/18 ? 0 07/25/18 500
 .
 PSC
 filename=account
-records=0000000000002
-ldbname=bank
-timestamp=2018/09/16-19:04:37
+records=0000000000003
+ldbname=absdb
+timestamp=2018/09/16-21:38:44
 numformat=44,46
 dateformat=mdy-1950
 map=NO-MAP
 cpstream=ISO8859-1
 .
-0000000456
+0000000697
