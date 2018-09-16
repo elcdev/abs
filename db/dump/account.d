6298b35928641c5d6d66195f8cf74405dfc225f1
allegorijab <allegorijab@gmail.com>
2018-09-16 19:05:52 +0300

    Initial test data

---
+++ db/dump/account.d
@@ -0,0 +1,14 @@
+1 0 0 2018-09-08T03:12:16.927 "user" 2018-09-08T03:12:16.927 "user" 10212000 "" "CBLT.EUR" "CBLT.EUR" "" "EUR" "" "DFB" "SCA" "" "" "0" 0 "ACTIVE" "CENTRAL BANK OF LITHUANIA (Clients Funds)" 07/25/18 ? 0 07/25/18 07/25/18 500 ? 0
+2 0 0 2018-09-08T03:18:37.170 "user" 2018-09-08T03:18:37.170 "user" 30311000 "" "123456789012" "123456789012" "" "EUR" "123" "CIF" "" "" "1" "0" 0 "Active" "Test client Account" 07/25/18 07/25/18 0 07/25/18 ? 0 07/25/18 500
+.
+PSC
+filename=account
+records=0000000000002
+ldbname=bank
+timestamp=2018/09/16-19:04:37
+numformat=44,46
+dateformat=mdy-1950
+map=NO-MAP
+cpstream=ISO8859-1
+.
+0000000456
