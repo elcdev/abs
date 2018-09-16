02aa750211c9ea4073af2ee4b34b59ab523cb370
cido <cidocido@gmail.com>
2018-09-16 21:43:05 +0300

    Test data

---
+++ db/dump/closed_days.d
@@ -0,0 +1,13 @@
+1 83 0 2018-09-12T02:00:51.342 "media" 2018-09-12T02:00:35.478 "user" 09/12/18 no
+.
+PSC
+filename=closed_days
+records=0000000000001
+ldbname=absdb
+timestamp=2018/09/16-21:42:44
+numformat=44,46
+dateformat=mdy-1950
+map=NO-MAP
+cpstream=ISO8859-1
+.
+0000000086
