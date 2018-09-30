085b1f91d500e0dc3e760af086e3036d4a21b147
allegorijab <allegorijab@gmail.com>
2018-10-01 01:14:29 +0300

    Changes in transactionLineModel.cls and new model clientModel.cls

--- system/api/core/requestItemGetDbByTwoField.f
+++ system/api/core/requestItemGetDbByTwoField.f
@@ -1,7 +1,7 @@
         DEFINE BUFFER {&table} FOR {&table}.
         
         FIND FIRST {&table} NO-LOCK WHERE {&table}.{&field} = i{&field} 
-		and {&table}.{&field2} = i{&field2} NO-ERROR.
+		and {&table}.{&field2} = i{&field2} and {&table}.state <> 9 NO-ERROR.
         IF NOT AVAILABLE {&table} THEN RETURN "RECORD-NOT-FOUND".
 		
 		id      = {&table}.id.
